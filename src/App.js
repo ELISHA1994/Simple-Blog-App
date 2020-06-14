04f90cf5aa17afd09955d3b035fecbd672ce8d71
elishabello <elishabello2014@gmail.com>
2020-06-14 21:53:36 +0100

    LogoutComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import Login from './user/Login'
+import Logout from "./user/Logout";
 
 export default function App () {
-  return <Login />
+  return <Logout user="Elisha Bello" />
 }
