065d4b6ff18422edc3ff966e4dc8f10544e8b6f3
elishabello <elishabello2014@gmail.com>
2020-06-15 13:05:48 +0100

    User State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import PostList from './post/PostList'
 import CreatePost from "./post/CreatePost"
 import UserBar from "./user/UserBar";
 
-const user = 'Elisha Bello'
 const posts = [
   { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
 ]
 
 export default function App () {
-  return (
-      <div style={{ padding: 8 }} >
-        <UserBar />
-        <br/>
-        <CreatePost user={user} />
-        <br/>
-        <hr />
-        <PostList posts={posts} />
-      </div>
-  )
+
+    const [ user, setUser ] = useState('')
+    return (
+        <div style={{ padding: 8 }} >
+            <UserBar user={user} setUser={setUser} />
+            <br/>
+            {user && <CreatePost user={user} />}
+            <br/>
+            <hr />
+            <PostList posts={posts} />
+        </div>
+    )
 }
