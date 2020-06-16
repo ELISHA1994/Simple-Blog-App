f63757853238929ecc992826158d412b69567445
elishabello <elishabello2014@gmail.com>
2020-06-16 01:55:04 +0100

    post reducer hooks

--- src/App.js
+++ src/App.js
@@ -20,16 +20,26 @@ function userReducer (state, action) {
     }
 }
 
+
+function postsReducer(state, action) {
+    switch (action.type) {
+        case 'CREATE_POST':
+            const newPost = { title: action.title, content: action.content, author: action.author }
+            return [newPost, ...state]
+        default: throw new Error()
+    }
+}
+
 export default function App () {
 
     const [ user, dispatchUser ] = useReducer( userReducer, '')
-    const [ posts, setPosts ] = useState(defaultPosts)
+    const [ posts, dispatchPosts ] = useReducer(postsReducer, defaultPosts)
 
     return (
         <div style={{ padding: 8 }} >
             <UserBar user={user} dispatch={dispatchUser} />
             <br/>
-            {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
+            {user && <CreatePost user={user} posts={posts} dispatch={dispatchPosts} />}
             <br/>
             <hr />
             <PostList posts={posts} />
@@ -38,7 +48,12 @@ export default function App () {
 }
 
 
-// Actions
+// ........................Actions...........................
+// ..........user Actions..................
 // { type: 'LOGIN', username: 'Daniel Bugl', password: 'notsosecure' }
 // { type: 'REGISTER', username: 'Daniel Bugl', password: 'notsosecure', passwordRepeat: 'notsosecure' }
 // { type: 'LOGOUT' }
+
+// ............post Actions...................
+// { type: 'CREATE_POST', title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' }
+
